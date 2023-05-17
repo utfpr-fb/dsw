6a66ec8ff17e5b3c4087cf5c017a72745cb31131
Adair Rohling <rohling@users.noreply.github.com>
2023-05-17 05:06:34 +0000

    Dashboard OK

--- dsw20231/app/layout.tsx
+++ dsw20231/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js" async />
     </html>
   )
 }
